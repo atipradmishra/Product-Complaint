3a16a41f23094591d1fccea5c79eca5463817eac
atipradmishra <atipradmishra@gmail.com>
2025-05-21 18:50:22 +0530

    aws key removel

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 DB_NAME = "product_poc.db"
 BUCKET_NAME = "product-complaint-mgnt"
 
-aws_access_key = "ASIAQUF"
-aws_secret_key = "im+nKWaJqo/1mCIK3ib/M"
+aws_access_key = ""
+aws_secret_key = ""
 
 
 FERNET_KEY = b'10IBWLcBK_zIaAYmjNes8Y9Fl4oK1FPMcFd0SOC6mIQ='
